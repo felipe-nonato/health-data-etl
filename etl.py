780c9e4c1e88cc5c586ee551a6cc9485c1b658b1
Felipe Nonato <felipe.tavares.nonato@gmail.com>
2025-04-04 19:01:14 -0300

    ajuste app running

--- etl.py
+++ etl.py
@@ -31,7 +31,7 @@ def run_etl():
     
     # Step 3: Start API
     print("Iniciando API...")
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     run_etl()
