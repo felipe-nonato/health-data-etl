ff6da27cabfd97dcdcf3ce47034145913c301b5a
Felipe Nonato <felipe.tavares.nonato@gmail.com>
2025-04-04 21:51:57 -0300

    fix(frontend): ajustando requisições do frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,7 +19,7 @@ const paginatedResults = computed(() => {
 
 async function fetchSearchResults(query) {
   try {
-    const response = await axios.get(`https://etl-api.felipenonato.com:5000/operadoras?query=${encodeURIComponent(query)}`);
+    const response = await axios.get(`https://etl-api.felipenonato.com/operadoras?query=${encodeURIComponent(query)}`);
 
     // Manipula diretamente o JSON retornado
     if (Array.isArray(response.data)) {
